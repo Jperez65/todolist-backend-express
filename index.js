26e6fea0d5680e509952eb009164604ceaac7249
Josue <josuefour@gmail.com>
2021-11-14 17:03:11 -0500

    added cors and fixed crashing issue

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 import express, { request } from 'express';
 import PrismaClient from '@prisma/client'
+import cors from 'cors';
 
 
 const prisma = new PrismaClient.PrismaClient();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.PORT;
 
+// Event Listeners 
+process.on('uncaughtException', (error, origin) => {
+    console.log('uncauht exception');
+    console.log(error);
+    console.log('exception origin');
+    console.log(origin);
+    console.log( `app is listening http://localhost:${port}`);
+});
+
+process.on('unhandledRejection', (reason, promised) => {
+    console.log('Unhandled Rejection at ');
+    console.log(promise);
+    console.log('Reason');
+    console.log(reason);
+    console.log( `app is listening http://localhost:${port}`);
+});
 
 app.get('/', (req, res)=> {
     res.send('todolist application is running ');
 });
 
 app.get("/getall/", async (req, res) => {
-    const job = await prisma.todo.findMany().catch((error)=>{
-        console.log(error);
-        res.send(error);
+    const todo = await prisma.todo.findMany().catch((error) => {
+        res.send(`An error occured: ${error.message}`);
     });
-    res.json(job);
-    res.send('Get all have been fetched')
+    res.json(todo);
   });
 
 app.post('/api/todolist/create', async (req, res) => {
-    const todo = await prisma.todo.create({ data: req.body }).catch((error) =>{
-        console.log(error);
-        res.send(error);
+    const todo = await prisma.todo.create({ data: req.body }).then(() => {
+        res.send('To Do item was created');
+    }).catch((error) =>{
+        res.send(`An error occured: ${error.message}`);
     });
-    res.send('To Do item was created');
 });
 
-
-
-app.delete('/api/todolist/delete/:id', async (req, res)=>{
+app.delete('/api/todolist/delete/:name', async (req, res)=>{
     const deleteUser = await prisma.todo.delete({
         where: {
-          id: parseInt(req.params.id),
+          name: req.params.name,
         },
+    }).then(() => {
+        res.send(`To Do item ${req.params.name} is deleted`);
     }).catch((error) => {
-        console.log(error);
-        res.send(error);
+        res.send(`An error occured: ${error.message}`);;
     });
-    res.send(`To Do item ${req.params.id}`);
 });
 
 app.listen(port, () => {
